e1d95856693f0854ea5adc904d1b1fc79ada456b
thingsmade <martin@thingsmade.co.za>
2017-06-20 09:34:18 +0200

    Cleaned up a bit

--- chars.py
+++ chars.py
@@ -61,7 +61,10 @@ def getConfusion(t1,t2):
     
     
 if __name__ == '__main__':
-    inp=data.load_data('data.npz')
+    filen=data.filen
+    if len(sys.argv)>1:
+        pass
+    inp=data.load_data(filen)
     trainnr,row,col=inp[0].shape
     testnr,ig1,ig2=inp[2].shape
     if not (ig1==row and ig2==col):
@@ -71,47 +74,39 @@ if __name__ == '__main__':
     y_train =keras.utils.to_categorical(inp[1],CLASSES)
     x_test=inp[2].reshape(testnr, row, col, 1)
     y_test=keras.utils.to_categorical(inp[3],CLASSES)
-    print y_test
-#    x_train /= 255
-#    x_test /= 255
 
     input_shape = (row, col, 1)
 
-    if False:
+    if len(sys.argv)==1:
         m=model1(input_shape,10)
         m.fit(x_train, y_train,
                   batch_size=1024,
                   epochs=20,
                   verbose=1,
                   validation_data=(x_test, y_test))
-        m.save_weights("play1.wts")
+        m.save_weights("model.wts")
     else:
+        
         m=model1(input_shape,10)
-        m.load_weights("play.wts")
+        m.load_weights("model.wts")
         
     r=m.predict(x_test)
-    print r.shape,x_test.shape
-    print r.sum(axis=1)
     rr=getConfusion(r,y_test)
+    print 'Confusion matrix'
     for i in range(0,CLASSES):
+        print "class %i :"%i,
         for j in range(0,CLASSES):
             print rr[j,i],
         print
-    print rr.sum(axis=0)
-    print rr.sum(axis=1)
-    print r.shape,rr.shape,y_test.shape
-    print r[0]
     score = m.evaluate(x_test, y_test, verbose=0)
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
     
-    print(y_test,y_test[0],y_test[0,0])
-    print(score)
     max=np.argmax(r, 1)
     zz=np.zeros_like(y_test)
     zz[np.arange(0,10000),max]=1.0
-    print zz.shape,max.shape,y_test.shape
-    print max
-    print zz
     rr=zz*y_test
+    print "classification accuracy per character"
     print rr.sum(axis=0)/y_test.sum(axis=0)
+    print "classification accuracy"
+    print rr.sum()/y_test.sum()
--- data.py
+++ data.py
@@ -5,7 +5,8 @@ Created on 16 Jun 2017
 '''
 import _mysql as ms
 import numpy as np
-
+import sys
+filename='data.npz'
 def get_table(db,name,count=None,block=1000):
     if count is None:
         db.query("SELECT count(*) from %s"%name)
@@ -36,10 +37,23 @@ def shape_data(raw):
     return np.reshape(raw[:,1:],(raw.shape[0],28,28)),raw[:,0]
 
 if __name__ == '__main__':
-    if True:
-        db=ms.connect(user='martin',db='mnist1',host='127.0.0.1',passwd='')
+    filen=filename    
+    if len(sys.argv)>1:
+        user='martin'
+        db='mnist1'
+        host='127.0.0.1'
+        passwd=''
+        try:
+            user  = sys.argv[1]
+            db    = sys.argv[2]
+            host  = sys.argv[3]
+            passwd= sys.argv[4]
+            filen  = sys.argv[5]
+        except:
+            pass
         
-    #    train=get_table(db,'mnist_train',count=10,block=2)
+        db=ms.connect(user=user,db=db,host=host,passwd=passwd)
+        
         train=get_table(db,'mnist_train')
         test=get_table(db,'mnist_test')
         for i in range(0,10):
@@ -49,13 +63,9 @@ if __name__ == '__main__':
         x_train,y_train=shape_data(train)
         x_test,y_test=shape_data(test)
         outd={}
-    #    outd["x_train"]=x_train
-    #    outd["y_train"]=y_train
-    #    outd["x_test"]=x_test
-    #    outd["y_test"]=y_test
-        np.savez("dataa.npz",x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test)
+        np.savez(filen,x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test)
         print x_train.shape,x_train.shape
-    aa=np.load("mnist.npz")
+    aa=np.load(filen)
     print aa.files
     print aa["x_train"].shape
     print aa["y_train"].shape
